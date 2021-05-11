82ac63701b51f3f8e5c75c7fbd9c5e4b4676bd3f
wang jia <wangjia_1919@163.com>
2021-05-11 14:01:02 +0800

    add aqs

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -14,5 +14,31 @@ module.exports = {
       // use more markdown-it plugins!
       // md.use(require('markdown-it-katex'))
     }
+  },
+
+  themeConfig: {
+    repo: 'NineSwordsMonster',
+    docsDir: 'docs',
+
+    editLinks: true,
+    editLinkText: 'Edit this page on GitHub',
+    lastUpdated: 'Last Updated',
+
+    algolia: {
+      apiKey: '380c0fa62872c88c54072130d9e6e78d',
+      indexName: 'Github Page'
+    },
+
+    nav: [
+      { text: '首页', link: '/', activeMatch: '^/$|^/guide/' },
+      {
+        text: '博客',
+        link: '/blogs/',
+        activeMatch: '^/blogs/'
+      }
+    ],
+
+    sidebar: {
+    }
   }
-};
+}
